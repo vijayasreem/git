a34967c821ac65a4787862d46f6f90212a8a2a64
dotnet-code <@dotnet-code>
2023-06-02 11:51:14 +0530

    commit from code202306025114

---
+++ git/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,29 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  signUp(): void {
+    try {
+      if (this.validateForm()) {
+        // Sign up code
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  validateForm(): boolean {
+    // Validation code
+    return true;
+  }
+}
